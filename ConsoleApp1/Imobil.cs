0534c371bcc0623bd7e4dadd46736c73c53ace12
Staicu Adrian <adrianstacu888@gmail.com>
2020-02-27 22:05:36 +0200

    Add project files.

---
+++ ConsoleApp1/Imobil.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Imobil
+    {
+        public int camere;
+        public int etaj;
+        public float suprafata;
+
+        public void baseImobil()
+        {
+            Console.Write("Camere " + camere);
+            Console.WriteLine("Etaj " + etaj);
+            Console.WriteLine("Suprafata " + suprafata);
+        }
+    }
+
+    class Apartament : Imobil
+    {
+        public int locparcare;
+        public void park()
+        { 
+            Console.WriteLine("Apartament fara parcare");
+        }
+    }
+
+    class Casa : Imobil
+    {
+        public string gradina;
+        public void sweet()
+        {
+            Console.WriteLine("Casa cu gradina");
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Apartament a1 = new Apartament();
+            a1.camere = 3;
+            a1.etaj = 2;
+            a1.suprafata = 60.5f;
+            a1.locparcare = 0;
+            a1.baseImobil();
+            a1.park();
+
+            Casa a2 = new Casa();
+            a2.camere = 4;
+            a2.etaj = 2;
+            a2.suprafata = 80.8f;
+            a2.gradina = "gradia cu flori";
+            a2.baseImobil();
+            a2.sweet();
+
+            Console.ReadKey();
+        }
+    }
+}
